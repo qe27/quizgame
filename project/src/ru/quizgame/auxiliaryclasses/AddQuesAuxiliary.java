f44467243476795440774a1fe9e8aebd62729206
qe27 <myfakemail345@gmail.com>
2015-05-12 14:02:30 +0400

    Добавил вспомогательный класс

---
+++ project/src/ru/quizgame/auxiliaryclasses/AddQuesAuxiliary.java
@@ -0,0 +1,14 @@
+
+package ru.quizgame.auxiliaryclasses;
+
+public class AddQuesAuxiliary {
+    static private int correct=0;
+	static public int getCorrect (){
+		return correct;
+	}
+	static public void setCorrect (int c) {
+		correct = c;
+	}
+        
+        
+}
